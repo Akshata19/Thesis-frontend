4042e42a955d0ad012ab78950eb7020151a3a741
Akshata19 <akshatamithari@gmail.com>
2025-04-21 23:49:27 +0200

    updated chatbot endpoint

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
   backendUrl: 'https://thesis-backend-production-6496.up.railway.app',
-  rasaEndpoint:
-    'https://thesis-rasa-production.up.railway.app/webhooks/rest/webhook',
+  rasaEndpoint: 'https://chatbots.railway.internal/webhooks/rest/webhook',
 };
